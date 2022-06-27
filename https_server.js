10e3c3bd59a96e606f184ab502f7769936137526
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-06-27 16:30:23 -0400

    ...

--- https_server.js
+++ https_server.js
@@ -16,16 +16,33 @@ async function main() {
     let server = https.createServer(options, async (req, res) => {
 
         console.log(req.url)
+        if (req.url.endsWith('.html')) {
 
-        res.writeHead(200, {
-            "Content-type": "text/html"
-        });
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            })
+            let html = fs.readFileSync(__dirname + req.url, "utf8")
+            const user = "Node JS"
+            
+            html = html.replace("{ user }", user)
+            res.end(html)
+
+        } else if (req.url.endsWith('.jpeg')) {
+
+            res.writeHead(200, {
+                "Content-type": "image/jpeg"
+            })
+            let jpeg = fs.readFile(__dirname + req.url, (err, data) => {
+                res.end(data)
+            })
+
+        } else {
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            })
+            res.end('TEMP')
+        }
         
-        let html = fs.readFileSync(__dirname + req.url, "utf8");
-        const user = "Node JS";
-        
-        html = html.replace("{ user }", user);
-        res.end(html);
 
     })
 
