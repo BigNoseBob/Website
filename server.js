4e0e7a4d9542883c37196ef0d1c428066e461323
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-06-27 01:42:44 -0400

    fixes

--- server.js
+++ server.js
@@ -7,20 +7,25 @@ const fs = require('node:fs')
 async function main() {
 	
     // HTTP Server
-    const port = 6000
-    const html = fs.readFileSync('./portfolio.html')
+    const port = 8888
 
     let server = http.createServer(async (req, res) => {
 
         res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        res.end(html)
+            "Content-type": "text/html"
+        });
+        
+        let html = fs.readFileSync(__dirname + "/portfolio.html", "utf8");
+        const user = "Node JS";
+        
+        html = html.replace("{ user }", user);
+        res.end(html);
 
     })
 
     server.listen(port)
     console.log(`Listening on http://ec2-3-22-234-91.us-east-2.compute.amazonaws.com:${port}`)
+    // console.log(`Listening on http://localhost:${port}`)
 
 }
 
