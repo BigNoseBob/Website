61420f5027b14cc78737edeebc5fa1b3862b2a88
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-06-28 10:48:51 -0400

    IP lookup

--- https_server.js
+++ https_server.js
@@ -6,10 +6,12 @@ const fs = require('node:fs')
 const axios = require('axios')
 
 const ipstack_api_key = 'c9dfba0314861072e8989dc2e5c77a3a'
+const ipgeolocation_api_key = '8b1cd26a9bab447788846b4b07aa3852'
 
 async function ip_lookup(ipv4) {
 
-    const url = `https://api.ipstack.com/${ipv4}?access_key=${ipstack_api_key}`
+    // const url = `https://api.ipstack.com/${ipv4}?access_key=${ipstack_api_key}`
+    const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${ipgeolocation_api_key}&ip=${ipv4}`
     let res = await axios({ method: 'GET', responseType: 'json', url: url }).catch(err => console.error(err))
     return res.data
 
