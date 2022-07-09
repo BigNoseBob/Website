41f5e40f5a97d5311fde31d15e237929a5361e95
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-07-09 17:33:31 -0400

    Added buymeacoffee button

--- https_server.js
+++ https_server.js
@@ -73,7 +73,7 @@ async function main() {
     })
 
     server.listen(port)
-    console.log(`Listening on https://oliverr.dev:${port}`)
+    console.log(`Listening on https://localhost:${port}`)
 
 }
 
--- js/gort/title.js
+++ js/gort/title.js
@@ -26,6 +26,7 @@ export async function main() {
     let title = document.getElementById('gort_title')
     let description = document.getElementById('description')
     let left = document.getElementById('left')
+    let buymeacoffee = document.getElementById('buymeacoffee')
 
     let guild_count = document.getElementById('guild_count')
     let user_count = document.getElementById('user_count')
@@ -44,6 +45,7 @@ export async function main() {
 
     description.style.animation = 'fadeIn 1s forwards'
     left.style.animation = 'fadeIn 1s forwards'
+    buymeacoffee.style.animation = 'fadeIn 1s forwards'
 
 
 }
