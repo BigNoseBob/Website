2586ff8fb2a9b70553241cec2b6b0bd214327d37
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-07-13 10:57:21 -0400

    Gort API troubleshooting

--- API/API_request.js
+++ API/API_request.js
@@ -79,6 +79,7 @@ async function initialize() {
         } else {
             // It's an API call
 
+            console.log(endpoint)
             if (!(gort_api_keys.includes(headers["authorization"])) && (url != '/' && endpoint != '/gort/spotify/link' )) {
                 res.writeHead(400, {
                     "Content-Type": "text/json"
