02000ba95e15ebf823feaf2cc8f6925a55028280
Oliver Rayner <oliver.e.rayner@gmail.com>
2022-07-13 10:47:56 -0400

    Gort API troubleshooting

--- API/endpoints/spotify_link.js
+++ API/endpoints/spotify_link.js
@@ -12,7 +12,10 @@ module.exports = {
     async execute({ url, headers }) {
         
         const querystring = url.substring(url.indexOf('?'))
-        const res = await axios({ method: 'POST', url: 'http://localhost:4078' + querystring })
+        const headers = {
+            Authorization: "6Q0otRbN7gchxX5Pdi0aA6fs7LmZd8xXKXDislO6",
+        }
+        const res = await axios({ method: 'POST', url: 'http://localhost:4078' + querystring, headers: headers })
 
     }
 
